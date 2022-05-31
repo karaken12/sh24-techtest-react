11ce2184c1712a9e800ba4b0e7e974338ab6dea3
Kara Potts <karaken12@gmail.com>
2022-06-01 00:32:36 +0100

    Clean up isPostcodeShippable routine
    Make it clearer how the postcode calculation is done.

--- src/isPostcodeShippable.tsx
+++ src/isPostcodeShippable.tsx
@@ -1,16 +1,25 @@
-export const isPostcodeShippable = async (postcode: string) => {
-  const allowedPostcodes = ["SH241AA", "SH241AB"];
+const allowedPostcodes = ["SH241AA", "SH241AB"];
+const allowedLsoas = ["Southwark", "Lambeth"];
 
+export const isPostcodeShippable = async (postcode: string) => {
   const strippedPostcode = postcode.toUpperCase().replaceAll(/[^A-Z0-9]/g, "");
 
   // If on the allow-list, don't even make the API call
-  if (allowedPostcodes.includes(strippedPostcode)) return true;
+  if (postcodeIsOnAllowList(strippedPostcode)) return true;
 
+  return lsoaIsShippable(await getLsoaForPostcode(strippedPostcode));
+};
+
+const postcodeIsOnAllowList = (strippedPostcode: string) =>
+  allowedPostcodes.includes(strippedPostcode);
+
+const lsoaIsShippable = (lsoa: string) =>
+  allowedLsoas.some((allowedLsoa) => lsoa.startsWith(allowedLsoa + " "));
+
+const getLsoaForPostcode = async (strippedPostcode: string) => {
   const url = `https://api.postcodes.io/postcodes/${strippedPostcode}`;
   const response = await fetch(url);
-  if (response.status != 200) return false;
+  if (response.status != 200) return "";
   const jsonResponse = await response.json();
-  const lsoa: string = jsonResponse.result.lsoa;
-
-  return lsoa.startsWith("Southwark ") || lsoa.startsWith("Lambeth ");
+  return jsonResponse.result.lsoa;
 };
