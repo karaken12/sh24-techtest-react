b061d96248d12da26fcd19c50c22f66f19a7adf7
Kara Potts <karaken12@gmail.com>
2022-05-31 20:34:14 +0100

    Add basic form elements

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders a form with postcode input", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const inputElement = screen.getByLabelText(/postcode/i);
+  expect(inputElement).toBeInTheDocument();
+  expect(inputElement).toHaveAttribute("type", "text");
+
+  const submitElement = screen.getByText(/check postcode/i);
+  expect(submitElement).toBeInTheDocument();
+  expect(submitElement).toHaveAttribute("type", "submit");
 });
