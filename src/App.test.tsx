11a3d2f6ae7bd06fe788dd40cd3fbb1a0b9627fc
Kara Potts <karaken12@gmail.com>
2022-05-31 22:06:39 +0100

    Add submit button test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("renders a form with postcode input", () => {
   render(<App />);
@@ -12,4 +13,15 @@ test("renders a form with postcode input", () => {
   const submitElement = screen.getByText(/check postcode/i);
   expect(submitElement).toBeInTheDocument();
   expect(submitElement).toHaveAttribute("type", "submit");
+});
+
+test("submitting the form keeps the value in the input", () => {
+  render(<App />);
+
+  const inputElement = screen.getByLabelText(/postcode/i);
+  userEvent.type(inputElement, "SE1 7QD");
+  const submitElement = screen.getByText(/check postcode/i);
+  userEvent.click(submitElement);
+
+  expect(screen.getByLabelText(/postcode/i)).toHaveValue("SE1 7QD");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./App.css";
 
+function PostcodeForm() {
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <label id="postcode-label">
+        Postcode:
+        <input name="postcode" type="text" />
+      </label>
+      <input type="submit" value="Check postcode" />
+    </form>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <form>
-        <label id="postcode-label">
-          Postcode:
-          <input name="postcode" type="text" />
-        </label>
-        <input type="submit" value="Check postcode" />
-      </form>
+      <PostcodeForm />
     </div>
   );
 }
